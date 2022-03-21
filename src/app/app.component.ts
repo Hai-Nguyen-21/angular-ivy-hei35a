b996f5b2ab6497094610f8c0898777277c20698d
Hai-Nguyen-21 <hainlph17388@gmail.com>
2022-03-21 16:38:41 +0700

    output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,10 @@
-import {
-  Component,
-  OnChanges,
-  OnInit,
-  SimpleChanges,
-  VERSION,
-} from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent{
-  
-
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
-
-  currentProgress = 0;
-  isHide = false;
-
-  upToProgress() {
-    this.currentProgress = this.currentProgress + 10;
-    if (this.currentProgress == 100) {
-      this.isHide = !this.isHide;
-    }
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { AuthorDetailComponent } from './author-details/author-details.component';
+import { AuthorListComponent } from './author-list/author-list.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, ProgressBarComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    AuthorListComponent,
+    AuthorDetailComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
